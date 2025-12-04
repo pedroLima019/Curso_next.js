907f0dab5830a5e475ba2c02a79d22d3f10339a5
pedroLima019 <pedroliveiralima21@gmail.com>
2025-12-04 14:15:40 -0300

    create news projects

---
+++ projeto01/my-app/src/app/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center p-4 w-full ">
+      <div className="container mx-auto flex flex-col justify-center items-center gap-5">
+        <Image
+          src="/task.jpg"
+          width={500}
+          height={500}
+          alt="imagem tarefas"
+          objectFit="cotain"
+          priority
+        />
+        <div className="w-full flex flex-col  justify-center items-center gap-3">
+          <h1 className="text-3xl font-black">
+            Bem vindo ao sistemas de gerenci amento de tarefas
+          </h1>
+          <p className="text-xl ">Organize suas tarefas</p>
+        </div>
+
+        <div className="w-full flex justify-center items-center space-x-5">
+          <span className="bg-zinc-900 p-3 w-[200px]  rounded-2xl text-white text-center hover:bg-zinc-800 duration-300 transition-all">
+            +5mil posts
+          </span>
+          <span className="bg-zinc-900 p-3 w-[200px]  rounded-2xl text-white text-center  hover:bg-zinc-800 duration-300 transition-all">
+            +10mil comentários
+          </span>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ projeto01/my-app/src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>TodoDias+</h1>
+    </header>
+  );
+};
+
+export default Header;
